a029dc8ed4a5d0bea61032ecbd217a050acaf62c
JoshanWadekar <joshanwadekar994@gmail.com>
2024-11-15 09:49:43 +0530

    The changes

--- cal.py
+++ cal.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to the Simple Calculator!"
+    return "Welcome to the Simple Calculator! HPCAP  CDAC Pune "
 
 @app.route('/add', methods=['GET'])
 def add():
